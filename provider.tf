601575dd5b33b697b5a4cf023e0c7d90eeb3ec5d
Hick Tan <hicktan@gmail.com>
2026-01-07 17:39:05 -0300

    feat:Adicionado provider com tags

---
+++ provider.tf
@@ -0,0 +1,29 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+  #Para o desafio comentei o backend, para produção deve ser criado um bucket S3 
+  #antes da implementação
+  # backend "s3" {
+  #   bucket  = "desafio-cloudscript"
+  #   key     = "state/terraform.tfstate"
+  #   region  = "us-east-1"
+  #   encrypt = true
+  #   use_lockfile = true
+  # }
+}
+
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = {
+      owner      = "hicktan"
+      managed-by = "terraform"
+      projetct   = "desafio-cloudscript"
+    }
+  }
+}
